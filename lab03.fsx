ea8a15b66fcb123a35bf90031f9bc5d18f30f38e
Kamil <kamilcz2001@gmail.com>
2023-11-23 12:28:58 +0100

    Drzewo

--- lab03.fsx
+++ lab03.fsx
@@ -114,4 +114,193 @@ let zad7 = fun lista ->
             loop (suma + head) (liczbaElementow + 1) ogon
     loop 0 0 lista
 
-printfn "zad7 %A" (zad7 lista)
+//printfn "zad7 %A" (zad7 lista)
+
+let zad8 = fun (lista: Lista<string>) (separator: string) ->
+    let rec loop = fun nowyString -> 
+        function
+        | Pusta -> nowyString
+        | Wezel(head, ogon) ->
+            if ogon = Pusta then
+                loop (nowyString + head) ogon
+            else
+                loop (nowyString + head + separator) ogon
+    loop "" lista
+
+let tablicaString = Wezel("Ala", Wezel("ma", Wezel("kota", Pusta)))
+
+//printfn "Zad8: %A" (zad8 tablicaString " ")
+
+let zad9 = fun (lista: Lista<string>) (pozycja: int) ->
+    let rec loop = fun (n: int) ->
+        function
+        | Pusta -> failwith "Nie znaleziono danej pozycji"
+        | Wezel(glowa, ogon) ->
+            if n = pozycja then
+                String.length(glowa)
+            else
+                loop (n+1) ogon
+    loop 1 lista
+
+//printfn "Zad8: %A" (zad9 tablicaString 3)
+
+let zad10 = fun (lista: Lista<string>) ->
+    let rec loop = fun (min: string) (max: string) ->
+        function
+        | Pusta -> (min, max)
+        | Wezel(glowa, ogon) ->
+            if String.length(glowa) > String.length(max) then
+                loop min (glowa) ogon
+            elif String.length(glowa) < String.length(min) then
+                loop (glowa) (max) ogon
+            else
+                loop (min) (max) ogon
+    match lista with
+    | Pusta -> ( "", "" )  // Obsługa przypadku pustej listy
+    | Wezel (pierwszyElement, resztaListy) ->
+        loop pierwszyElement pierwszyElement resztaListy
+
+let listaStringow = Wezel("Ala", Wezel("ma", Wezel("kota", Wezel("Kamil", Wezel("K", Pusta)))))
+
+//printfn "Lista10: %A" (zad10 listaStringow)
+
+// inne rozwiazanie
+let zad10v2 (lista: Lista<string>) =
+    let rec loop min max = function
+        | Pusta -> (min, max)
+        | Wezel (glowa, ogon) ->
+            let dlugoscGlowy = String.length glowa
+            let nowyMin = if dlugoscGlowy < String.length min then glowa else min
+            let nowyMax = if dlugoscGlowy > String.length max then glowa else max
+            loop nowyMin nowyMax ogon
+
+    match lista with
+    | Pusta -> failwith "Lista nie może być pusta."
+    | Wezel (pierwszyElement, resztaListy) ->
+        loop pierwszyElement pierwszyElement resztaListy
+        
+let zad11 (listaImion: Lista<string>) (nowaLista: Lista<string>) =
+    // Funkcja pomocnicza, która przechodzi przez listę
+    let rec loop (lista: Lista<string>)  (nowaLista: Lista<string>) =
+        match lista with
+        | Pusta -> nowaLista
+        | Wezel(glowa, ogon) ->
+            // Sprawdź, czy ostatnia litera imienia to 'a'
+            if glowa.[glowa.Length - 1] = 'a' then
+                // Dodaj imię do nowej listy
+                loop ogon (Wezel(glowa, nowaLista))
+            else
+                // Pomijaj imię i kontynuuj przetwarzanie
+                loop ogon nowaLista
+
+    // Wywołaj funkcję pomocniczą z początkowymi danymi
+    loop listaImion Pusta
+
+let imiona = Wezel("Kamil", Wezel("Ania", Wezel("Kasia", Wezel("Asia", Wezel("Mateusz", Pusta)))));
+let nowaLista = Pusta
+
+printfn "Zadanie 11: %A" (zad11 imiona nowaLista)
+
+let rec zad12 (lista: Lista<'a>) : Lista<'a> =
+    let rec pomocnicza (wejscie: Lista<'a>) (wyjscie: Lista<'a>) =
+        match wejscie with
+        | Pusta -> wyjscie
+        | Wezel(glowa, ogon) -> pomocnicza ogon (Wezel(glowa, wyjscie))
+    
+    pomocnicza lista Pusta
+
+let rec zad13 (listaImion: Lista<string>) (listaZ: Lista<string>) (listaM: Lista<string>) = 
+    match listaImion with
+    | Pusta -> (listaZ, listaM)
+    | Wezel(head, tail) ->
+    if head.[head.Length - 1] = 'a' then
+        zad13 tail (Wezel(head, listaZ)) listaM
+    else
+        zad13 tail listaZ (Wezel(head, listaM))
+
+//printfn "zad13: %A" (zad13 imiona Pusta Pusta)
+
+let rec zad14 (lista1: Lista<int>) (lista2: Lista<int>) : Lista<bool> =
+    match lista1, lista2 with
+    | Pusta, Pusta -> Pusta
+    | _, Pusta | Pusta, _ -> failwith "Listy muszą być tej samej długości"
+    | Wezel(glowa1, ogon1), Wezel(glowa2, ogon2) ->
+        let wiekszaNaPierwszejLiscie = glowa1 > glowa2
+        Wezel(wiekszaNaPierwszejLiscie, zad14 ogon1 ogon2)
+
+// Zdefiniuj typ wyliczeniowy do reprezentacji wyników porównania
+type WynikPorownania =
+    | Pierwsza
+    | Druga
+
+let rec zad15 (lista1: Lista<int>) (lista2: Lista<int>) : Lista<WynikPorownania> =
+    match lista1, lista2 with
+    | Pusta, Pusta -> Pusta
+    | _, Pusta -> Wezel(Pierwsza, zad15 Pusta lista1)
+    | Pusta, _ -> Wezel(Druga, zad15 Pusta lista2)
+    | Wezel(glowa1, ogon1), Wezel(glowa2, ogon2) ->
+        if glowa1 > glowa2 then
+            Wezel(Pierwsza, zad15 ogon1 lista2)
+        else if glowa1 < glowa2 then
+            Wezel(Druga, zad15 lista1 ogon2)
+        else
+            Wezel(Pierwsza, zad15 ogon1 ogon2) 
+
+type KierunekSortowania =
+    | Rosnaco
+    | Malejaco
+
+let rec zad16 (lista: Lista<'a>) (kierunek: KierunekSortowania) : bool =
+    match lista with
+    | Pusta | Wezel(_, Pusta) -> true
+    | Wezel(glowa1, Wezel(glowa2, ogon)) ->
+        match kierunek with
+        | Rosnaco -> glowa1 <= glowa2 && zad16 (Wezel(glowa2, ogon)) Rosnaco
+        | Malejaco -> glowa1 >= glowa2 && zad16 (Wezel(glowa2, ogon)) Malejaco
+
+let rec zad17 (lista1: Lista<int>) (lista2: Lista<int>) : Lista<int> =
+    match lista1, lista2 with
+    | Pusta, _ -> lista2
+    | _, Pusta -> lista1
+    | Wezel(glowa1, ogon1), Wezel(glowa2, ogon2) ->
+        if glowa1 <= glowa2 then
+            Wezel(glowa1, zad17 ogon1 lista2)
+        else
+            Wezel(glowa2, zad17 lista1 ogon2)
+
+// Zdefiniuj typ elementu stosu
+type Stos<'a> =
+    | Pusty
+    | Element of 'a * Stos<'a>
+
+// Funkcja dodająca element na stos
+let push (element: 'a) (stos: Stos<'a>) : Stos<'a> =
+    Element(element, stos)
+
+// Funkcja usuwająca element ze stosu
+let pop (stos: Stos<'a>) : 'a * Stos<'a> =
+    match stos with
+    | Pusty -> failwith "Stos jest pusty"
+    | Element(glowa, ogon) -> glowa, ogon
+
+// Funkcja sprawdzająca, czy stos jest pusty
+let isEmpty (stos: Stos<'a>) : bool =
+    match stos with
+    | Pusty -> true
+    | _ -> false
+
+let stosPusty = Pusty
+let stos1 = push 1 stosPusty
+let stos2 = push 2 stos1
+
+let (element, nowyStos) = pop stos2
+printfn "Zdjety element: %d" element
+
+let czyPusty = isEmpty nowyStos
+printfn "Stos jest pusty: %b" czyPusty
+
+// DRZWA BINARNE
+
+type Drzewo =
+    | Puste
+    |  Wezel of float * Drzewo * Drzewo
